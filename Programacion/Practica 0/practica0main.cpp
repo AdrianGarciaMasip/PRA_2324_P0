366988199ba28cbca7aacc348dc1dc80b3f1b5ea
Adrián García Masip <upvadrian@gmail.com>
2023-09-20 19:53:58 +0200

    Creo que todo acabado, falta comprobar

--- Programacion/Practica 0/practica0main.cpp
+++ Programacion/Practica 0/practica0main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <practica0.h>
 
 using namespace std;
 
 int main(){
-	BrazoRobotico();
+	BrazoRobotico Br = BrazoRobotico(15.5, 20.43, 17.4);
+
+	Br.sujetando();
+	Br.coger();
+	cout << Br.consultx() << endl;
+	cout << Br.consulty() << endl;
+	cout << Br.consultz() << endl;
+	cout << consultsujetando() << endl;
+
+	
+	return 0;
+
 }
